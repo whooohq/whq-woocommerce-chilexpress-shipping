61417665921478d1c0f6c68ccbd20eb9ec7eff35
Esteban <esteban.git@actitud.xyz>
2018-05-02 16:39:45 -0300

    1.3.11

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -28,3 +28,27 @@ function whq_wcchp_cart_enable_city( $array ) {
 		add_filter( 'woocommerce_shipping_calculator_enable_city', '__return_true' );
 	}
 }
+
+/**
+ * Prevents order with Chilexpress at zero cost
+ */
+add_action( 'woocommerce_checkout_order_processed', 'whq_wcchp_prevent_chilexpress_zerocost_order' );
+function whq_wcchp_prevent_chilexpress_zerocost_order( $order_id ) {
+	$ship_with_chilexpress = false;
+
+	foreach ( $_POST['shipping_method'] as $value ) {
+		if ( stripos( $value, 'chilexpress' ) !== false ) {
+			$ship_with_chilexpress = true;
+		}
+	}
+
+	if( $ship_with_chilexpress === true ) {
+		$order          = wc_get_order( $order_id );
+		$order_shipping = $order->calculate_shipping();
+
+		if( $order_shipping <= 0 ) {
+			//Can't be Chilexpress and zero shipping cost at the same time
+			throw new Exception( 'Lo sentimos, Chilexpress no se encuentra disponible en estos momentos. Por favor, seleccione otro método de envío.' );
+		}
+	}
+}
