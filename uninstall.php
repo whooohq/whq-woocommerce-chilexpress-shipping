c737d097e4efdb113d8d9e1399d7ab504e90f318
Esteban <esteban@attitude.cl>
2022-03-07 17:49:10 -0300

    1.5.0

--- uninstall.php
+++ uninstall.php
@@ -3,21 +3,21 @@
  * WooCommerce Chilexpress Shipping uninstall
  */
 
-if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
 global $wpdb;
 
-//Delete plugin's options
+// Delete plugin's options
 $options_leftovers = array(
 	'whq_wcchp_incompatible_plugins',
 );
 
-foreach ($options_leftovers as $option) {
+foreach ( $options_leftovers as $option ) {
 	delete_option( $option );
 }
 
-//Delete plugin's transients
+// Delete plugin's transients
 $wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE ('\_transient%\_whq_wcchp\_%');" );
 $wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE ('\_transient_timeout%\_whq_wcchp\_%');" );
